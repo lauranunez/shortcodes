9b1f3a5dc4a26490ade197222ba297e418ac2438
Laura Nuñez <30507934+lauranunez@users.noreply.github.com>
2017-08-26 17:09:19 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,42 @@
+
+
+// Shortcode Buttom 
+add_shortcode( 'button', 'emo_button_shortcode' );
+ 
+function emo_button_shortcode( $atts ) {
+       extract( shortcode_atts(
+               array(
+                       'title' => 'Title',
+                       'url' => ''
+               ),
+               $atts
+       ));
+       return '<span class="emo-button"><a href="' . $url . '">' . $title . '</a></span>';
+}
+
+// Shortcode Enclosing
+function EMO_shortcode($atts = [], $content = null)
+{
+    // do something to $content
+ 
+    // always return
+    return '<span class="emo-code">'. $content . '</span>';
+}
+add_shortcode('codigo', 'EMO_shortcode');
+
+<style>
+.emo-button {
+padding: 12px;
+font-size: 18px;
+border-radius: 2px;
+border:2px solid #d26e4b;
+	background: #d26e4b;
+
+}
+.emo-button a {
+text-decoration: none;
+color:#fff !important;
+	text-transform: uppercase;
+	font-family:Roboto, Arial,"sans-serif";
+}
+</style>
